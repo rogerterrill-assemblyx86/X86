d4a89762572c03a47745b0a49de4945ddc79a5ae
Roger Terrill <rchicasterill@gmail.com>
2019-01-31 16:18:51 -0800

    Programming Exercise 3.2

--- ch03/main.asm
+++ ch03/main.asm
@@ -1,33 +1,31 @@
-TITLE Subtracting Three Integers	   (AddSub.asm)
+TITLE Data Definitions	   (AddSub.asm)
 
-COMMENT ! 1. Subtracting Three Integers
-    Using the AddSub program from Section 3.2 as a reference, write a program that subtracts three
-    integers using only 16-bit registers. Insert a call DumpRegs statement to display the register
-    values.
+COMMENT ! 2. Data Definitions
+    Write a program that contains a definition of each data type listed in Table 3-2 in Section 3.4.
+    Initialize each variable to a value that is consistent with its data type.
 !
 
 INCLUDE Irvine32.inc
 
+
+.data
+
+val1 BYTE 'A'
+val2 SBYTE -128
+val3 WORD 0FFFFh
+val4 SWORD 8402h
+val5 DWORD 0FFFFFFFFh
+val6 SDWORD -80000000h
+val7 FWORD 281474976710655
+val8 QWORD 1234567812345678h
+val9 TBYTE 1208925819614629174706175
+val10 REAL4 -1.2
+val11 REAL8 3.2E-260
+val12 REAL10 4.6E+4096
+
 .code
 main PROC
 
-    mov	  ax,9000h	   ; AX = 9000h
-    sub	  ax,4000h	   ;	AX = 5000h
-    sub	  ax,2000h	   ; AX = 3000h
-
-    mov	  bx,9000h	   ; BX = 9000h
-    sub	  bx,4000h	   ;	BX = 5000h
-    sub	  bx,2000h	   ; BX = 3000h
-
-    mov	  cx,9000h	   ; CX = 9000h
-    sub	  cx,4000h	   ;	CX = 5000h
-    sub	  cx,2000h	   ; CX = 3000h
-
-    mov	  dx,9000h	   ; DX = 9000h
-    sub	  dx,4000h	   ;	DX = 5000h
-    sub	  dx,2000h	   ; DX = 3000h
-
-    call	  DumpRegs
 
     exit
 main ENDP
