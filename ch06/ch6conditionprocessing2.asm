78204441239d5372e91d0cf0e30a7129f6404554
RogerDodger <lpaddikt@gmail.com>
2019-02-03 11:27:42 -0800

    Ch6 program 2

--- ch06/ch6conditionprocessing2.asm
+++ ch06/ch6conditionprocessing2.asm
@@ -1,21 +1,39 @@
-TITLE Encryption Program		(Encrypt.asm)
+TITLE Countdown		(ch6conditionprocessing2.asm)
 
-; This program demonstrates simple symmetric
-; encryption using the XOR instruction.
+COMMENT !
+	There are several program fragments or complete programs in the rest of the chapter.
+
+	Pick one of them and type it in and get it working.
+
+	After you get the program in and working, add something else that is interesting to it.
+
+	Tell me what you added to the program.
+!
+
+; Counts down from number inputed with a while conditional
 
 INCLUDE Irvine32.inc
 
 .data
-ranNum DWORD ?
+val1	DWORD	?
+val2	DWORD	?
+prompt	BYTE	"Please type in a number between 1-200: ",0
 
 .code
 main PROC
-	mov eax,100
-	call RandomRange
-	mov ranNum, eax
-	mov eax, ranNum
-	call WriteInt
-	call Crlf
+	mov	edx,OFFSET prompt
+	call WriteString
+	call ReadInt
+
+	.WHILE eax > val1
+		call	WriteDec
+		DEC		eax
+		mov		val2,eax
+		mov		eax,50
+		call	Delay
+		mov		eax,val2
+		call	Crlf
+	.ENDW
 
 	exit
 main ENDP
